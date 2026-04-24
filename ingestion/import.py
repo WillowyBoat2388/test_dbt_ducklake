6edd748d7b7c8431efb8f4f920b91592b396923b
WillowyBoat2388 <onidajo99@gmail.com>
2026-04-24 23:33:55 +0100

    feat: uv package manager addition for environment development

---
+++ ingestion/import.py
@@ -0,0 +1,13 @@
+import duckdb
+import os
+from loguru import logger
+from dotenv import load_dotenv
+
+
+
+def ingest_from_api(data_path=None):
+    """Ingest data from API and load into Ducklake."""
+
+    
+
+ 
