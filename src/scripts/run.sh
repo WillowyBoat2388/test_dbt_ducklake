9b6cb2cfeb745c2caf349eb71cfc1202e1fe814d
WillowyBoat2388 <onidajo99@gmail.com>
2026-05-05 07:19:19 +0100

    fix: Image Update

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -6,8 +6,10 @@ echo "Running DUCKLAKE PIPELINE"
 echo "Run all dependencies to collect the required packages before ducklake pipeline execution"
 uv sync
 echo "Dependencies Installed"
-cd src
 
+cd ./src
+echo "Changed directory to src"
+ls
 
-uv run ./etl/ingestion/main.py
-./scripts/run_dbt.sh
+uv run etl/ingestion/main.py
+scripts/run_dbt.sh
